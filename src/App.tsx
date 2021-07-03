e3b25d5b6919be4a5fb97a24ec4c0fadf298a484
Gilang Al Khansa <gilang.khansa@shopee.com>
2021-07-03 20:27:38 +0700

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import {
+  Redirect,
+  Route,
+  BrowserRouter as Router,
+  Switch,
+} from 'react-router-dom';
+
 import './App.css';
+import QuestionsPage from './containers/Questions';
 
-import Questions from './containers/Questions';
-
-function App() {
-  return (
-    <div className="App">
-      <Questions />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Router>
+      <Switch>
+        <Route
+          path="/question/one"
+          component={QuestionsPage}
+        />
+        <Route
+          path="/question/two"
+          component={QuestionsPage}
+        />
+        <Route path="/">
+          <Redirect to="/question/one" />
+        </Route>
+      </Switch>
+    </Router>
+  </div>
+);
 
 export default App;
